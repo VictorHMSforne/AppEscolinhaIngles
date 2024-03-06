7f31823835ca92c967d873569473ce6ec8aa7f3f
Victor <vmendessforne@gmail.com>
2024-03-06 11:12:38 -0300

    Adicionar arquivos de projeto.

---
+++ EscolinhaIngles/EscolinhaIngles/MainPage.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace EscolinhaIngles
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCores_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new PageCores());
+        }
+
+        private void btnNumeros_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new PageNumeros());
+        }
+
+        private void btnFamilia_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new PageFamilia());
+        }
+
+        private void btnNumeros_Clicked_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnSair_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                System.Diagnostics.Process.GetCurrentProcess().Kill();
+            }
+            catch 
+            {
+
+            }
+        }
+    }
+}
